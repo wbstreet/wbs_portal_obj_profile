3d9dfc0b61bdb828302812d5893b68bda243d005
Konstantin Polyakov <kostyan_93@mail.ru>
2018-02-10 01:16:51 +0300

    different corrections

--- actions/view.php
+++ actions/view.php
@@ -11,6 +11,54 @@ if ($modPortalArgs['obj_id'] === null) { // здесь у нас не profile_id
     $modPortalArgs['obj_id'] = $admin->get_user_id();
 }
 
+function get_info($clsModPortalObj, $method, $obj_id) {
+    $objs = [];
+    $obj_count = 0;
+    $obj_exists = true;
+
+    if ($clsModPortalObj->obj_type_is_active === '1') {
+
+        // количество записей
+    
+        $obj_count = $clsModPortalObj->$method([
+            'user_owner_id'=>$modPortalArgs['obj_id'],
+            'is_active'=>1,
+            'is_deleted'=>0
+            ], true);
+        if (gettype($blog_count) === 'string') { $clsModPortalObjBlog->print_error($obj_count); $obj_exists = false;}
+    
+        // первые пять записей
+        
+        $_objs = $clsModPortalObj->$method([
+            'user_owner_id'=>$modPortalArgs['obj_id'],
+            'is_active'=>1,
+            'is_deleted'=>0,
+            'limit_count'=>5,
+            'order_by'=>'date_created',
+            'order_dir'=>'DESC',
+            ]);
+        if (gettype($_objs) === 'string') { $clsModPortalObj->print_error($_objs); $obj_exists = false;}
+        while($_objs !== null && $obj = $_objs->fetchRow()) {
+    
+            // вынимакем страницу блога
+    
+            $r = select_row(TABLE_PREFIX.'pages', '`link`', '`page_id`='.process_value($obj['page_id']));
+            if (gettype($r) === 'string') $clsModPortalObj->print_error($r);
+            else if ($r == null) $clsModPortalObj->print_error('Страница не найдена!');
+            else {
+                $r = $r->fetchRow();
+                $obj['obj_url'] = page_link($r['link'])."?obj_id=".$obj['obj_id'];
+            }
+    
+            $objs[] = $obj;
+        }
+
+    }
+    
+    return [$objs, $obj_count, $obj_exists];
+
+}
+
 ?>
 
 <?php
@@ -74,19 +122,64 @@ if ($modPortalArgs['obj_id'] === 'list') {
             echo "Пользователь отключил свой аккаунт.";
         } else {
             
+            // получаем специализацию
+            
             $skills = [];
             $r = $clsModPortalObjProfile->get_skills(['user_id'=>$admin->get_user_id()]);
             if (gettype($r) === 'string') {$clsModPortalObjProfile->print_error($r); $r = null; }
             while($r !== null && $row = $r->fetchRow()) $skills[] = $row;
 
-            $login_when = DateTime::createFromFormat('U', $profile['login_when']);
-            $profile['login_when'] = $login_when->format('r');
+            // получаем блоги
 
+            $path_blog = WB_PATH.'/modules/wbs_portal_obj_blog/lib.class.portal_obj_blog.php';
+            if (file_exists($path_blog)) {
+                include_once($path_blog);
+                $clsModPortalObjBlog = new ModPortalObjBlog($section_id, $page_id);
+                list($blogs, $blog_count, $blog_exists) = get_info($clsModPortalObjBlog, 'get_publication', $modPortalArgs['obj_id']);
+            } else { list($blogs, $blog_count, $blog_exists) = [[], 0, false]; }
+            
+            // получаем проекты
+            
+            $path_project = WB_PATH.'/modules/wbs_portal_obj_project/lib.class.portal_obj_project.php';
+            if (file_exists($path_project)) {
+                include_once($path_project);
+                $clsModPortalObjProject = new ModPortalObjProject($section_id, $page_id);            
+                list($projects, $project_count, $project_exists) = get_info($clsModPortalObjProject, 'get_project', $modPortalArgs['obj_id']);
+            } else { list($projects, $project_count, $project_exists) = [[], 0, false]; }
+
+            // получаем недвижимости
+            
+            $path_apartment = WB_PATH.'/modules/wbs_portal_obj_estate/lib.class.portal_obj_estate.php';
+            if (file_exists($path_apartment)) {
+                include_once($path_apartment);
+                $clsModPortalObjEstate = new ModPortalObjEstate($section_id, $page_id);            
+                list($apartments, $apartment_count, $apartment_exists) = get_info($clsModPortalObjEstate, 'get_apartment', $modPortalArgs['obj_id']);
+            } else { list($apartments, $apartment_count, $apartment_exists) = [[], 0, false]; }
+
+            // конвертируем дату предыдущего входа
+
+            $profile['login_when'] = DateTime::createFromFormat('U', $profile['login_when']);
+            
             // отображаем
 
             $clsModPortalObjProfile->render('view.html', [
                'profile'=>$profile,
                'skills'=>$skills,
+
+               'blogs'=>$blogs,
+               'blog_count'=>$blog_count,
+               'blog_exists'=>$blog_exists,
+               //'blog_max_count'=>$blog_max_count,
+
+               'projects'=>$projects,
+               'project_count'=>$project_count,
+               'project_exists'=>$project_exists,
+               //'project_max_count'=>$project_max_count,
+    
+               'apartments'=>$apartments,
+               'apartment_count'=>$apartment_count,
+               'apartment_exists'=>$apartment_exists,
+               //'apartment_max_count'=>$apartment_max_count,    
             ]);
             
         }
